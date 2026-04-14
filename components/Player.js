e813c311e423aa56e79508a3ba645954077fce2e
PosiBears <keepgoodmind@gmail.com>
2026-04-15 04:27:29 +0800

    update

--- components/Player.js
+++ components/Player.js
@@ -42,7 +42,7 @@ const Player = () => {
       setPlayer(
         new window.APlayer({
           container: ref.current,
-          fixed: true,
+          fixed: false,
           lrcType: lrcType,
           autoplay: autoPlay,
           order: order,
